09143af93ec9bc0687bb3bc9a8c4819b5da2d831
Ryan Mathews <mathewsryan2@gmail.com>
2025-11-03 09:35:54 -0600

    Added gitignore and setup env variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg"); //handles the connection between psql and node
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "Discs",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = {
